f9548365ce8717316c63b011d27b9333d455de4e
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-06-18 21:52:46 +0200

    injecting data from API inside the HTML

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const CLEFAPI = '42b979ab919db9d989124f4906c77c57'
 let resultatsAPI;
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const heure = document.querySelectorAll('.heure-nom-prevision');
+const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -19,6 +24,34 @@ function AppelAPI(long,lat) {
         return reponse.json();
     })
     .then((data) => {
-        console.log(data);
+       
+        resultatsAPI = data;
+        temps.innerText = resultatsAPI.current.weather[0].description;
+        temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`
+        localisation.innerText = resultatsAPI.timezone;
+        
+        // Les heures par tranches de 3 avec leurs temperature
+
+        let = heureActuelle = new   Date().getHours();
+
+        for (let i = 0; i < heure.length; i++) {
+            
+            let heureInc = heureActuelle + i * 3;
+
+            if (heureInc > 24) {
+                heure[i].innerText = `${heureInc - 24} h`      
+            } else if (heureInc === 24 ) {
+                heure[i].innerText = "00 h"
+            } else {
+                heure[i].innerText = `${heureInc} h`;
+            }
+        }
+
+        // temp pour 3h
+
+        for (let j = 0; j < tempPourH.length; j++) {
+            tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`
+        }
+        
     })
 }
